bb78f4de65756b96f5d7ecafe7ca63f4c2fcdec8
vikasrai19 <vikasrai1906@gmail.com>
2024-11-05 21:36:45 +0530

    update: new fall back added

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,19 @@
 'use client'
 
 import { LandingScene } from "@/components/landing_scene";
+import { RocketLoader } from "@/components/model_components/rocket_loader";
 import { Stars } from "@/components/stars";
 import { useQueryConfig } from "@/config/useQuery.config";
 import { useGetData } from "@/hooks/useGetData";
 import { ScrollControls, useScroll } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
+import { Suspense } from "react";
+import { useMediaQuery } from "react-responsive";
 
 
 export default function Home() {
+
+  const isMobile = useMediaQuery({ maxWidth: 768 })
 
   const { data: eventsData, isLoading: isEventsLoading } = useGetData(
     'eventsList',
@@ -18,7 +23,10 @@ export default function Home() {
   return (
     <Canvas>
       <ScrollControls pages={10} >
-        <LandingScene eventsData={eventsData} />
+        <Suspense fallback={<RocketLoader />} >
+
+          <LandingScene eventsData={eventsData} />
+        </Suspense>
         <Stars />
       </ScrollControls>
     </Canvas >
