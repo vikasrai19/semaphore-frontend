ecb63d46d6dd8288a266e7abdf7b2bc8c38438f8
vikasrai19 <vikasrai1906@gmail.com>
2024-11-13 21:07:28 +0530

    update: removed console.log statements

--- src/app/event-heads/update-scores/page.js
+++ src/app/event-heads/update-scores/page.js
@@ -33,8 +33,6 @@ export default function UpdateScores() {
       useQueryConfig
     );
 
-  console.log('event details ', eventTeamDetails)
-
   useEffect(() => {
     if (eventTeamDetails) {
       let newList = [];
